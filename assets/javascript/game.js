9ebef6d96e79355be27e970fdb30b4ec2c1c7108
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-02-17 22:50:27 -0600

    Changed random var to magicNumber var. Reorganized JS code and updated notes.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,54 @@
-//On page load...
+// On page load...
 $(document).ready(function () {
 
-    var sapphire = 2;
-    var garnet = 2;
-    var topaz = 2;
-    var emerald = 2;
+    // The following variables are declared globally.
     var score = 0;
-    // Computer chooses random number
-    var random = 19;
+    var magicNumber = 19;
+    var randGemVal = 12;
 
-    var randFunct = function () {
-        random = Math.floor(Math.random() * 102) + 19;
-        $(".magicNum").html("Magic Number: " + random);
+    // Computer chooses random number.
+     var randFunct = function () {
+        magicNumber = Math.floor(Math.random() * 102) + 19;
+        $(".magicNum").html("Magic Number: " + magicNumber);
     };
     randFunct();
 
-    randGemVal = 12;
-
-    var gemValFunct = function () {
+    // Function returns random values between 2 and 12 to be assigned to gems.
+     var gemValFunct = function () {
         randGemVal = Math.floor(Math.random() * 11) + 2;
         return randGemVal;
-    }
+    };
+
+    // Random values are assigned to gems.
     var sapphireVal = gemValFunct();
     var garnetVal = gemValFunct();
     var topazVal = gemValFunct();
     var emeraldVal = gemValFunct();
 
-    console.log(randGemVal);
-
-    // User is able to click on gems.
+    // User is able to click on gems to reach the magic number.
     $("#sapphire").click(function () {
         score += sapphireVal;
         $(".scoreDisplay").text("Your Current Score: " + score);
         console.log(sapphireVal);
     });
+
     $("#garnet").click(function () {
         score += garnetVal;
         $(".scoreDisplay").text("Your Current Score: " + score);
         console.log(garnetVal);
     });
+
     $("#topaz").click(function () {
         score += topazVal;
         $(".scoreDisplay").text("Your Current Score: " + score);
         console.log(topazVal);
     });
+
     $("#emerald").click(function () {
         score += emeraldVal;
         $(".scoreDisplay").text("Your Current Score: " + score);
         console.log(emeraldVal);
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
