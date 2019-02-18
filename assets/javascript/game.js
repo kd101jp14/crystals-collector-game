bab9bde4bacd1493a8903a89f36daf28a28c5997
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-02-17 22:07:44 -0600

    Created click events for gems.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //On page load...
-$(document).ready(function() {
+$(document).ready(function () {
 
     var sapphire = 2;
     var garnet = 2;
@@ -9,13 +9,46 @@ $(document).ready(function() {
     // Computer chooses random number
     var random = 19;
 
-    var randFunct = function() {
+    var randFunct = function () {
         random = Math.floor(Math.random() * 102) + 19;
         $(".magicNum").html("Magic Number: " + random);
     };
     randFunct();
 
-   
+    randGemVal = 12;
+
+    var gemValFunct = function () {
+        randGemVal = Math.floor(Math.random() * 11) + 2;
+        return randGemVal;
+    }
+    var sapphireVal = gemValFunct();
+    var garnetVal = gemValFunct();
+    var topazVal = gemValFunct();
+    var emeraldVal = gemValFunct();
+
+    console.log(randGemVal);
+
+    // User is able to click on gems.
+    $("#sapphire").click(function () {
+        score += sapphireVal;
+        $(".scoreDisplay").text("Your Current Score: " + score);
+        console.log(sapphireVal);
+    });
+    $("#garnet").click(function () {
+        score += garnetVal;
+        $(".scoreDisplay").text("Your Current Score: " + score);
+        console.log(garnetVal);
+    });
+    $("#topaz").click(function () {
+        score += topazVal;
+        $(".scoreDisplay").text("Your Current Score: " + score);
+        console.log(topazVal);
+    });
+    $("#emerald").click(function () {
+        score += emeraldVal;
+        $(".scoreDisplay").text("Your Current Score: " + score);
+        console.log(emeraldVal);
+    });
 
 
 
