605f5dc5fa20e9b2df429f1c3e697ab49ae93929
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-02-21 00:06:12 -0600

    Refactored click function for gems. Updated comments.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 // On page load...
 $(document).ready(function () {
-    // Audio begins to play.
+
+    // Audio plays once page is loaded.
     $("#mysteryMusic").get(0).play();
 
     // The following variables are declared globally.
@@ -10,40 +11,40 @@ $(document).ready(function () {
     var magicNumber = 19;
     var randGemVal = 12;
 
-    // Computer chooses random number.
-    var randFunct = function () {
+    // Function chooses random number, which will be assigned to the magic number.
+    var chooseMagicNum = function () {
         magicNumber = Math.floor(Math.random() * 102) + 19;
         $(".magicNum").html("Magic Number: " + magicNumber);
     };
-    randFunct();
+    chooseMagicNum();
 
     // Function returns random values between 2 and 12 to be assigned to gems.
-    var gemValFunct = function () {
+    var chooseGemVal = function () {
         randGemVal = Math.floor(Math.random() * 11) + 2;
         return randGemVal;
     };
 
     // Random values are assigned to gems.
-    var sapphireVal = gemValFunct();
-    var garnetVal = gemValFunct();
-    var topazVal = gemValFunct();
-    var emeraldVal = gemValFunct();
+    var sapphireVal = chooseGemVal();
+    var garnetVal = chooseGemVal();
+    var topazVal = chooseGemVal();
+    var emeraldVal = chooseGemVal();
 
-    // Gems are not allowed to have the same value.
+    // If gem value matches that of another gem, choose new value. (Needs to be refactored somehow...)
     if (sapphireVal === garnetVal || sapphireVal === topazVal || sapphireVal === emeraldVal) {
-        sapphireVal = gemValFunct();
+        sapphireVal = chooseGemVal();
     }
 
     if (garnetVal === sapphireVal || garnetVal === topazVal || garnetVal === emeraldVal) {
-        garnetVal = gemValFunct();
+        garnetVal = chooseGemVal();
     }
 
-    if (topazVal === sapphireVal ||topazVal ===garnetVal ||topazVal === emeraldVal) {
-       topazVal = gemValFunct();
+    if (topazVal === sapphireVal || topazVal === garnetVal || topazVal === emeraldVal) {
+        topazVal = chooseGemVal();
     }
 
     if (emeraldVal === sapphireVal || emeraldVal === topazVal || emeraldVal === garnetVal) {
-        emeraldVal = gemValFunct();
+        emeraldVal = chooseGemVal();
     }
 
     // User knows gems are clickable, as cursor becomes pointer when hovering.
@@ -51,40 +52,44 @@ $(document).ready(function () {
         $(this).css('cursor', 'pointer');
     });
 
-    // User can click on gems to attempt to match score w/ magic number.
-    $("#sapphire").click(function () {
-        score += sapphireVal;
-        $(".scoreDisplay").text("Your Current Score: " + score);
-        console.log(sapphireVal);
-    });
 
-    $("#garnet").click(function () {
-        score += garnetVal;
-        $(".scoreDisplay").text("Your Current Score: " + score);
-        console.log(garnetVal);
-    });
+    // Refactored click function for all gems. Score is appropriately updated when any gem is clicked.
+    $(".gem").click(function () {
+        var $el = $(this);
+        // When any gem is clicked, it is distinguished from the others by its ID.
+        var gemID = $el.attr("id");
+        console.log(gemID);
 
-    $("#topaz").click(function () {
-        score += topazVal;
-        $(".scoreDisplay").text("Your Current Score: " + score);
-        console.log(topazVal);
-    });
+        // Function for updating score.
+        var updateScore = function() {
+         score += gemVal[gemID];
+         $(".scoreDisplay").text("Your Current Score: " + score);
+        }
 
-    $("#emerald").click(function () {
-        score += emeraldVal;
-        $(".scoreDisplay").text("Your Current Score: " + score);
-        console.log(emeraldVal);
-    });
+        // Object organizes gems and their values.
+        var gemVal = {
+            sapphire: sapphireVal,
+            garnet: garnetVal,
+            topaz: topazVal,
+            emerald: emeraldVal,
+        };
+ 
+        gemVal[gemID]
+        console.log(gemVal[gemID])
 
+        // Nested function is called to update score.
+        updateScore(gemVal[gemID]);
+    });
+ 
     // Function allows game reset.
     var reset = function () {
         score = 0;
         $(".scoreDisplay").text("Your Current Score: " + score);
-        randFunct();
-        sapphireVal = gemValFunct();
-        garnetVal = gemValFunct();
-        topazVal = gemValFunct();
-        emeraldVal = gemValFunct();
+        chooseMagicNum();
+        sapphireVal = chooseGemVal();
+        garnetVal = chooseGemVal();
+        topazVal = chooseGemVal();
+        emeraldVal = chooseGemVal();
     };
 
     // Wins, losses, and reset are determined by score.
